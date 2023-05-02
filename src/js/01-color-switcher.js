626ebbd57c50fe1f30a08f4bcc40d7cfcda9a842
DanielPerdomoDev <dfperdomodev@gmail.com>
2023-05-01 22:55:58 -0500

    frist commit test

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,2 +1,22 @@
-const start = document.getElementById('start');
-const stop = document.getElementById('stop');
+const startBtn = document.getElementById('start');
+const stopBtn = document.getElementById('stop');
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+    startBtn.disabled = true;
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+  console.log(`Interval with id ${timerId} has stopped!`);
+});
---
+++ src/js/02-timer.js
@@ -0,0 +1,40 @@
+// Descrito en la documentación
+import flatpickr from 'flatpickr';
+// Importación adicional de estilos
+import 'flatpickr/dist/flatpickr.min.css';
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+flatpickr('#datetime-picker', options);
+
+const startBtn = document.querySelector('[data-start]');
+startBtn.disabled = true;
+startBtn.addEventListener('click', startCounter);
+
